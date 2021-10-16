841797225a451127bf840ec1984506afd73ffb60
John <jsanli@ucsc.edu>
2021-10-15 19:12:31 -0700

    created items

--- docs/_template/main.js
+++ docs/_template/main.js
@@ -1,13 +1,80 @@
-title = "";
+title = "Black Friday";
 
 description = `
+Shop Until You Drop
 `;
 
-characters = [];
 
-options = {};
+characters = [
+//a: tv
+ `
+llllll
+lLLLLl
+lLLLLl
+lLLLLl
+lLLLLl
+llllll
+ `,
+//b: xbox
+ `
+glllg
+lglgl
+llgll
+lglgl
+glllg
+ `,
+//c: shirt
+ `
+  bb
+ bbbb
+bbbbbb 
+  bb 
+  bb 
+  bb 
+ `,
+ //d: pants
+ `
+  yy
+ LLLL
+ L  L
+ L  L
+ LLLL
+ `,
+ //e: ring
+ `
+ rrrrr
+ rr rr
+ rr rr
+ rr rr
+ rr rr
+ rr rr
+ `
+
+];
+
+const G = {
+  WIDTH: 200,
+  HEIGHT: 200
+}
+
+options = {	
+  viewSize: {x: G.WIDTH, y: G.HEIGHT},
+  isCapturing: true,
+  isCapturingGameCanvasOnly: true,
+  captureCanvasScale: 2,
+  seed: 1,
+  isPlayingBgm: false,
+  isReplayEnabled: true,
+  isDrawingParticleFront: true,
+  theme: "simple"
+};
 
 function update() {
   if (!ticks) {
   }
+  char("a", 100, 100); //TV
+  char("b", 25, 25); //xbox
+  char("c", 130, 130); //shirt
+  char("d", 170, 170); //engagement ring
+  char("e", 170, 40); //pants
 }
